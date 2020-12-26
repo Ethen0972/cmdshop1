ebe0ea330f72e94243249510ac2435afe35f6def
Ｅｔｈｅｎ０９７２ <>
2020-12-26 12:14:29 +0800

    读

---
+++ src/test.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("请输入用户名");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+
+        System.out.println("请输入密码");
+        String password=sc.next();
+    }
+}
